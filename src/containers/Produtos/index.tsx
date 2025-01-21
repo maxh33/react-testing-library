56cf14b367779648a0f3eb2d598dbc16c9a992e6
maxh33 <contact@maxhaider.dev>
2025-01-21 15:50:28 -0300

    test: add tests for Produtos component
    - Added tests to verify Produtos component rendering
    - Added mock data for testing
    - Updated package.json and package-lock.json
    - Deleted old Produtos and styles files
    - Added new Produtos component and styles

--- src/containers/Produtos/index.tsx
+++ src/containers/Produtos/index.tsx
@@ -1,7 +1,7 @@
-import Produto from '../components/Produto'
-import { useGetJogosQuery } from '../services/api'
+import Produto from '../../components/Produto'
+import { useGetJogosQuery } from '../../services/api'
 
-import * as S from './styles'
+import * as S from '././styles'
 
 const Produtos = () => {
   const { data: jogos, isLoading } = useGetJogosQuery()
